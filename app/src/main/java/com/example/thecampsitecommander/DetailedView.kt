5d186f4a8aabfd7b2a7023ba09702a3c058934c1
ashleymonyela42-hub <ashleymonyela42@gmail.com>
2026-06-10 13:51:22 +0200

    initial commit

--- app/src/main/java/com/example/thecampsitecommander/DetailedView.kt
+++ app/src/main/java/com/example/thecampsitecommander/DetailedView.kt
@@ -30,18 +30,3 @@ class DetailedView : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TheCampsiteCommanderTheme {
-        Greeting("Android")
-    }
-}
