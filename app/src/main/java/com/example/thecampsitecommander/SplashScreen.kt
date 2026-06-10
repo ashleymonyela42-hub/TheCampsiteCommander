b74ff6d319c3af27ffc428961f6a687f2e0e3e4f
ashleymonyela42-hub <ashleymonyela42@gmail.com>
2026-06-10 13:44:32 +0200

    initial commit

--- app/src/main/java/com/example/thecampsitecommander/SplashScreen.kt
+++ app/src/main/java/com/example/thecampsitecommander/SplashScreen.kt
@@ -13,15 +13,35 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.thecampsitecommander.ui.theme.TheCampsiteCommanderTheme
 
-class MainActivity : ComponentActivity() {
+class SplashScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TheCampsiteCommanderTheme {
-
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
 }
 
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TheCampsiteCommanderTheme {
+        Greeting("Android")
+    }
+}
