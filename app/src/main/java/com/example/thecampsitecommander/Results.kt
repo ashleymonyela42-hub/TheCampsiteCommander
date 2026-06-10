e0ca6b12b439b4eef368a64bbdc30016c3ea114f
ashleymonyela42-hub <ashleymonyela42@gmail.com>
2026-06-10 14:31:07 +0200

    initial commit

--- app/src/main/java/com/example/thecampsitecommander/Results.kt
+++ app/src/main/java/com/example/thecampsitecommander/Results.kt
@@ -19,29 +19,9 @@ class Results : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TheCampsiteCommanderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TheCampsiteCommanderTheme {
-        Greeting("Android")
-    }
-}
